9e8e649352fd354826825d3be7ecb02772fc8cf2
Dave Dash <dave@pinterest.com>
2013-07-15 16:53:36 -0700

    A working setup.py file.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+import os
+
+__version__ = '0.1'
+markdown_contents = open(os.path.join(os.path.dirname(__file__),
+                                      'README.md')).read()
+
+setup(
+    name='wheeljack',
+    version= __version__,
+    scripts=['bin/{}'.format(p) for p in os.listdir('bin')],
+    long_description=markdown_contents,
+    install_requires=['blessings', 'PyYAML'],
+    url="https://github.com/davedash/wheeljack",
+    author="Dave Dash",
+    author_email="dd+github@davedash.com",
+    packages=['wheeljack']
+)
+
